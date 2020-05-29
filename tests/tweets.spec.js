180df85b147dd6d6c7187acb7937bfbe375e4302
erocookv5 <matteodicaragmgmail.com>
2020-05-29 19:12:28 +0200

    Testing only negative responses for GETs of <<tweets>> route:\n

--- tests/tweets.spec.js
+++ tests/tweets.spec.js
@@ -7,3 +7,43 @@ const app = require('../app')
 const User = require('../models/user');
 chai.use(chaiHttp);
 
+let mongoose = require("mongoose");
+let Tweet = require('../models/tweet');
+const should = chai.should();
+
+describe('Testing only negative responses for GETs of <<tweets>> route:\n', () => {
+    it('GET: it should get an empty array and status 200', (done) => {
+        chai.request(app)
+            .get('/tweets')
+            .end((err, res) => {
+                res.should.have.status(200);
+                res.body.should.be.a('array');
+                res.body.length.should.be.eql(0);
+                done();
+            });
+    });
+    it('GET/:id: it should get an empty array and status 200', (done) => {
+        chai.request(app)
+            .get('/tweets')
+            .end((err, res) => {
+                res.should.have.status(200);
+                res.body.should.be.a('array');
+                res.body.length.should.be.eql(0);
+                done();
+            });
+    });
+    it('GET/:id: it should get an error json and status 200', (done) => {
+        chai.request(app)
+            .get('/tweets' + 'a123b')
+            .end((err, res) => {
+                res.should.have.status(404);
+                res.body.should.be.eql({
+                    "error": {
+                        "status": 404
+                    },
+                    "message": "Not Found"
+                })
+                done();
+            });
+    });
+});
